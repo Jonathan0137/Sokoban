640fd5641f89ad8414e934924d215748b7a8af3c
unknown <tom.daudelin@mail.utoronto.ca>
2020-09-16 10:42:28 -0400

    added game select menu (only functionality is back button)

--- menu.py
+++ menu.py
@@ -33,7 +33,7 @@ def mainMenuScreen(window):
                                              manager=manager)
 
     button_to_status = {
-        #start_button : gameMenuScreen,
+        start_button : gameMenuScreen,
         option_button : optionsScreen,
         quit_button : exitGame
     }
@@ -50,6 +50,11 @@ def mainMenuScreen(window):
         time_delta = clock.tick(60)/1000.0
         
 
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_ESCAPE]:
+            return "exit"
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return "exit"
@@ -63,10 +68,6 @@ def mainMenuScreen(window):
                         return status_check
             manager.process_events(event)
 
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_ESCAPE]:
-            return "exit"
 
         manager.update(time_delta)
         manager.draw_ui(window)
@@ -118,11 +119,11 @@ def optionsScreen(window):
                 return "exit"
 
 
-            #CHECKS QUIT BUTTON
+            #CHECKS BUTTON INPUT
             if event.type == pygame.USEREVENT:
                 if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                     current_status = button_to_status[event.ui_element]
-                    return button_to_status[event.ui_element]
+                    return current_status
             manager.process_events(event)
 
         window.fill((255,255,255))
@@ -130,25 +131,49 @@ def optionsScreen(window):
         manager.draw_ui(window)
         pygame.display.update()
 
-    """Game menu selection"""
-    def gameMenuScreen(window):
-        manager = pygame_gui.UIManager((window.get_width(), window.get_height()))
+"""Game menu selection"""
+def gameMenuScreen(window):
+    manager = pygame_gui.UIManager((window.get_width(), window.get_height()))
 
-        background_image_file = open("Legend.jpg")
-        background_image = pygame.image.load(background_image_file) #LOADS BACKGROUND IMAGE
+    background_image_file = open("Legend.jpg")
+    background_image = pygame.image.load(background_image_file) #LOADS BACKGROUND IMAGE
 
-        #pygame.mixer.music.load('Journey.mp3') #LOADS MUSIC
-        #pygame.mixer.music.play(-1) #PLAYS MUSIC -1 MEANS IN LOOP
+    #pygame.mixer.music.load('Journey.mp3') #LOADS MUSIC
+    #pygame.mixer.music.play(-1) #PLAYS MUSIC -1 MEANS IN LOOP
 
 
-        start_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((window.get_width()//2 - window.get_width()//6, window.get_height()//4), (window.get_width()//3, window.get_height()//8)),
-                                                text='Start Game',
-                                                manager=manager)
+    continue_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((window.get_width()//2 - window.get_width()//6, window.get_height()//4), (window.get_width()//3, window.get_height()//8)),
+                                            text='Continue',
+                                            manager=manager)
 
-        option_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((window.get_width()//2 - window.get_width()//6, 2*window.get_height()//4), (window.get_width()//3, window.get_height()//8)),
-                                                text='Game Options',
-                                                manager=manager)
+    level_select_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((window.get_width()//2 - window.get_width()//6, 2*window.get_height()//4), (window.get_width()//3, window.get_height()//8)),
+                                            text='Level Select',
+                                            manager=manager)
 
-        quit_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((window.get_width()//2 - window.get_width()//6, 3*window.get_height()//4), (window.get_width()//3, window.get_height()//8)),
-                                                text='Exit Game',
-                                                manager=manager)
+    back_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((window.get_width()//2 - window.get_width()//6, 3*window.get_height()//4), (window.get_width()//3, window.get_height()//8)),
+                                            text='Back',
+                                            manager=manager)
+
+    clock = pygame.time.Clock()
+
+    while True: #THE LOOP THAT DOES THE CONSTANT USER INPUT CHECKS AND DRAWS
+        pygame.time.delay(10) #This is the function that creates a time delay of x milliseconds
+        time_delta = clock.tick(60)/1000.0
+        
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return "exit"
+
+
+            #CHECKS BUTTON INPUT
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    current_status = "back"
+                    return current_status
+            manager.process_events(event)
+
+        window.fill((255,196,0))
+        manager.update(time_delta)
+        manager.draw_ui(window)
+        pygame.display.update()
