cfe68adc31a99033645824aac4958f81062e7394
unknown <tom.daudelin@mail.utoronto.ca>
2020-09-17 10:35:02 -0400

    stashing sokoban.py in prep for merge conflict resolution

--- sokoban.py
+++ sokoban.py
@@ -3,38 +3,34 @@ from menu import *
 
 from worker import worker
 from Level import Level
+
+"""Dynamic Function calling"""
+def draw_game_based_on_status(status, arg):
+    return game_status_dictionary[status](arg)
+
 pygame.init()
 
-
 window = pygame.display.set_mode((1800, 1000))
+
 pygame.display.set_caption("Sokoban")
 
-
+game_status_dictionary = {
+    "MainMenu" : mainMenuScreen,
+    "Options" : optionsScreen,
+    #"GameMenu" : gameMenuScreen,
+    #"LevelSelect" : levelSelectScreen,
+    #"InGame" : inGameScreen
+}
 
 clock = pygame.time.Clock()
 
 man = worker(300, 410, 64, 64)
 
-run = True
-while run:
-    
+game_status = "MainMenu"
+while game_status != "exit":
     clock.tick(27) #refresh rate
-    
-    #run = mainMenu(window) #load main menu
-    
-    for event in pygame.event.get(): # exit button works
-        if event.type == pygame.QUIT:
-            run = False
+    game_status = draw_game_based_on_status(game_status, window) #load main menu
 
-    #ask level then perform the following to draw level
-    myLevel = Level(8)  #init Level Class given level
-    size = myLevel.screen_size() #get the correct screen size base on how many boxes there are
-    window = pygame.display.set_mode(size) #change the size of the screen
-    myLevel.draw_level(window) # draw the level
-
-
-
-    keys = pygame.key.get_pressed()#get key presses
 
 
 pygame.quit()
