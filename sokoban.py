b731f60099b543fc8fbf13bd54b0f90717c88298
jonathan0137 <jonathanbai0317@outlook.com>
2020-10-02 11:43:50 -0400

    implemented windows.py for resizeing windows

--- sokoban.py
+++ sokoban.py
@@ -1,5 +1,6 @@
 import pygame
 from menu import *
+from windows import *
 from inGame import *
 from worker import worker
 from Level import Level
@@ -13,18 +14,6 @@ def draw_game_based_on_status(status, window):
         status = status.split("/")[0]
     
     return game_status_dictionary[status](arg)
-
-def initWindow():
-    json_file = open("env.json", "r")
-    options_dict = json.load(json_file)
-    if(options_dict["resolution"] == "Fullscreen"):
-        window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-    else:
-        x = int(options_dict["resolution"].split("x")[0])
-        y = int(options_dict["resolution"].split("x")[1])
-        window = pygame.display.set_mode((x, y))
-    return window
-
 
 pygame.init()
 
